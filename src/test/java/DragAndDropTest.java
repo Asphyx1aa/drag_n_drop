f52cebbc252e588c810c5b58b45a8a0d56ec1526
Timur <tim.vlasov96@gmail.com>
2025-01-25 08:09:42 +0300

    created tests

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,6 +1,11 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDropTest {
     @BeforeAll
@@ -10,7 +15,18 @@ public class DragAndDropTest {
     }
 
     @Test
-    void DragElementsTest() {
+    void DragElementsWithActionsTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        actions().dragAndDrop($("#column-a"), $("#column-b")).perform();
+        $("#column-a").shouldHave(text("B"));
+    }
 
+    @Test
+    void DragElementsTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
     }
 }
